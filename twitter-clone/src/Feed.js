701deb9994716dfea911e1dd5230b3744e159513
RyanSWest <west56309@gmail.com>
2021-08-06 15:43:21 -0500

    adding flipmove

--- twitter-clone/src/Feed.js
+++ twitter-clone/src/Feed.js
@@ -2,7 +2,8 @@ import React , {useState, useEffect}from 'react';
 import './feed.css';
 import TweetBox  from './TweetBox';
 import Post from './Post';
-import db from './firebase'
+import db from './firebase';
+import FlipMove from 'react-flip-move';
 
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 
@@ -28,13 +29,8 @@ function Feed() {
                 <div className = "post__header">
 
                 </div>
-
-                {/* <div className = 'post__headerText'>
-                    <h3>Hey!!</h3><span>
-
-                        <VerifiedUserIcon className = 'post__badge'/>
-                    </span>
-                </div> */}
+                 <TweetBox/>
+                <FlipMove> 
                 {posts.map(post => (
                     
                     <Post
@@ -47,16 +43,10 @@ function Feed() {
                      />
                      
     ))}
-                {/* <Post
-                 displayName ="Zank Frappa"
-                username= 'Zappa'
-                text = "IT WORKS!"
-                avatar='https://www.bing.com/th?id=OIP.AO0OY95_OK5THisN9Cff_AHaHO&w=209&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2'
-                image= 'https://www.bing.com/th?id=OIP.nrilZjACn2hzR9WbDWZSOAHaE9&w=143&h=106&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2'
-                verified={true}
-                  /> */}
+    </FlipMove>
+               
             </div>
-            <TweetBox/>
+           
 
         </div>
     )
--- twitter-clone/src/Post.js
+++ twitter-clone/src/Post.js
@@ -1,5 +1,5 @@
 import { Avatar } from '@material-ui/core'
-import React from 'react'
+import React, { useRef, forwardRef} from 'react'
 import './post.css';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
@@ -8,7 +8,8 @@ import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 import './post.css';
 
-function Post({
+const Post = forwardRef(
+    ({
     displayName,
     username,
     verified,
@@ -17,9 +18,12 @@ function Post({
     text,
     image,
     avatar
-}) {
+},ref)=> {
     return (
-        <div className = "post">
+        <div className = "post"
+        ref = {useRef()}
+         
+         >
             <div className = "post__avatar">
                 <Avatar
                   src = {avatar}/>
@@ -30,8 +34,7 @@ function Post({
                     <div className = 'post__headerText'>
                         <h3>{displayName} <span className= 'post__headerSpecial'>
                             {verified && ( <VerifiedUserIcon className = 'post__badge'/>) }
-                            {/* <VerifiedUserIcon className = 'post__badge'/> */}
-                        </span>
+                         </span>
                         </h3>
                     </div>
                     <div className ='post__headerDescription'>
@@ -50,7 +53,8 @@ function Post({
             
         </div>
         
-    )
+    );
 }
+);
 
 export default Post
